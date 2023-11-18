93cb1dda83e15b211c8a5ea35d94d225a92635f5
Finite <33845166+finia2NA@users.noreply.github.com>
2023-11-19 00:31:45 +0100

    Websockets V1: Server and client

---
+++ websockets/client/jsclient.js
@@ -0,0 +1,59 @@
+const WebSocket = require('ws');
+
+const socket = new WebSocket('ws://localhost:3000');
+const clientID = Math.random().toString(36).substr(2, 9);
+let sharedText = '';
+
+function displayText(text) {
+  // clear console
+  console.clear();
+  // display text
+  console.log(text);
+}
+
+// This code runs when the connection is first opened.
+// It sends a message to the server to request the current text.
+// The response from the server is handled by the message event listener below.
+socket.addEventListener('open', function (event) {
+  console.log('Connected to the WebSocket server');
+  // Request the current text
+  socket.send(JSON.stringify({ type: 'get-text' }));
+});
+
+// This code runs when a message is received from the server.
+// It will display the text sent by the server.
+socket.addEventListener('message', function (event) {
+  const msg = JSON.parse(event.data);
+  if (msg.type === 'text') {
+    // Update the text
+    displayText(msg.data);
+    sharedText = msg.data;
+  }
+});
+
+// Function to call when the text is updated by the user
+function updateText(newText) {
+  // Send the updated text to the server
+  const textToSend = sharedText + newText;
+  socket.send(JSON.stringify({ type: 'update-text', data: textToSend }));
+}
+
+// main loop
+function main() {
+  // Get the current text from the user
+  const readline = require('readline');
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question('Enter text: ', (answer) => {
+    // Update the text
+    updateText(answer);
+    // Close the readline interface
+    rl.close();
+    // Call main again
+    main();
+  });
+}
+
+main();
---
+++ websockets/server/server.js
@@ -0,0 +1,47 @@
+const WebSocket = require('ws');
+
+// The text variable that will store the current text
+let sharedText = '';
+
+// Create a WebSocket server
+const wss = new WebSocket.Server({ port: 3000 });
+
+// Broadcast function to send data to all connected clients
+const broadcast = (data) => {
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
+};
+
+// Set up the WebSocket connection event
+wss.on('connection', (ws) => {
+  console.log('A new client connected.');
+
+  // Send the current text to the newly connected client
+  ws.send(JSON.stringify({ type: 'text', data: sharedText }));
+
+  // Set up the message event
+  ws.on('message', (message) => {
+    const msg = JSON.parse(message);
+
+    // Handle different types of messages
+    switch (msg.type) {
+      case 'get-text':
+        // Send the current text to the client
+        ws.send(JSON.stringify({ type: 'text', data: sharedText }));
+        break;
+      case 'update-text':
+        // Update the shared text
+        sharedText = msg.data;
+        // Broadcast the updated text to all clients
+        broadcast(JSON.stringify({ type: 'text', data: sharedText }));
+        break;
+      default:
+        console.log('Unknown message type:', msg.type);
+    }
+  });
+});
+
+console.log('Collaborative text WebSocket server is running on ws://localhost:3000');
